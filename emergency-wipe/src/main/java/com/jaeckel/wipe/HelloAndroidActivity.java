ddc007e314920bbc16a8c57340f200037078080f
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-10 10:33:07 +0200

    Enable/Disable Device Admin

--- emergency-wipe/src/main/java/com/jaeckel/wipe/HelloAndroidActivity.java
+++ emergency-wipe/src/main/java/com/jaeckel/wipe/HelloAndroidActivity.java
@@ -1,29 +1,68 @@
 package com.jaeckel.wipe;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import de.akquinet.android.androlog.Log;
 
 public class HelloAndroidActivity extends Activity {
 
-    /**
-     * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after
-     * previously being shut down then this Bundle contains the data it most
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  static final int            RESULT_ENABLE = 1;
+  private DevicePolicyManager mDPM;
+  private ActivityManager     mAM;
+  private ComponentName       mDeviceAdminSample;
 
-        // Initializes the logging
-        Log.init();
+  /**
+   * Called when the activity is first created.
+   * 
+   * @param savedInstanceState
+   *          If the activity is being re-initialized after previously being shut down then this Bundle
+   *          contains the data it most recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise
+   *          it is null.</b>
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        // Log a message (only on dev platform)
-        Log.i(this, "onCreate");
+    final DevicePolicyManager mDPM = (DevicePolicyManager) this.getSystemService(Context.DEVICE_POLICY_SERVICE);
 
-        setContentView(R.layout.main);
-    }
+    mDeviceAdminSample = new ComponentName(this, AdminReceiver.class);
+
+    // Initializes the logging
+    Log.init();
+
+    // Log a message (only on dev platform)
+    Log.i(this, "onCreate");
+
+    setContentView(R.layout.main);
+
+    Button enableDeviceAdminButton = (Button) findViewById(R.id.enable_device_admin);
+    enableDeviceAdminButton.setOnClickListener(new View.OnClickListener() {
+      public void onClick(View view) {
+        if (mDPM.isAdminActive(mDeviceAdminSample)) {
+          // disable here
+          Toast.makeText(HelloAndroidActivity.this, "Disabling Device Admin", Toast.LENGTH_LONG).show();
+          mDPM.removeActiveAdmin(mDeviceAdminSample);
+        } else {
+          // enable here
+
+          Toast.makeText(HelloAndroidActivity.this, "Enabling Device Admin", Toast.LENGTH_LONG).show();
+          // Launch the activity to have the user enable our admin.
+          Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+          intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mDeviceAdminSample);
+          intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                          "Additional text explaining why this needs to be added.");
+          startActivityForResult(intent, RESULT_ENABLE);
+        }
+      }
+    });
+  }
 
 }
-
